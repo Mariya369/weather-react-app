1a068ee3f125873c01b5ef8a9e1a6f745789fc43
Mariya369 <mariyadotkova555@gmail.com>
2024-06-04 09:53:52 +0200

    Added GitHub repository

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">Weather App      
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by <a href='https://www.mariyadotkova.com/' target='_blank' > Mariya Dotkova</a> and is {" "}
+        <a href='https://github.com/Mariya369/weather-react-app' target='_blank'>
+          open-sourced on GitHub.
+        </a>
+      </footer>
     </div>
   );
 }
