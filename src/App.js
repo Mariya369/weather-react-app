58a8f00858b6043d599984e1785ef21e217ae110
Mariya369 <mariyadotkova555@gmail.com>
2024-06-04 13:29:01 +0200

    Build HTML

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
-      <h1>Weather App</h1>
+      <Weather />
       <footer>
         This project was coded by <a href='https://www.mariyadotkova.com/' target='_blank' > Mariya Dotkova</a> and is {" "}
         <a href='https://github.com/Mariya369/weather-react-app' target='_blank'>
